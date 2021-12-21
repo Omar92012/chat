0d8eaafc44fb2d470173c1eebf0719bb9fd65e48
Omar92012 <96386154+Omar92012@users.noreply.github.com>
2021-12-21 12:50:24 -0500

    5 now means space

--- main.ts
+++ main.ts
@@ -1,19 +1,20 @@
-let messaage = ""
 input.onPinPressed(TouchPin.P0, function () {
-    messaage = "" + messaage + "-"
+    text_list.push(["5"])
 })
 input.onButtonPressed(Button.A, function () {
-    messaage = "" + messaage + "1"
+    text_list.push(1)
 })
 input.onButtonPressed(Button.AB, function () {
-    radio.sendString(messaage)
+    radio.sendString("" + (text_list))
 })
 radio.onReceivedString(function (receivedString) {
     basic.showString(receivedString)
 })
 input.onButtonPressed(Button.B, function () {
-    messaage = "" + messaage + "0"
+    text_list.push(0)
 })
+let text_list: number[] = []
+let list: number[] = []
 basic.forever(function () {
 	
 })
